f4ac0aa86586c06205a1c38fb285764ef616cdc9
Thomas Pendleton <thomas6576@yahoo.com>
2022-03-15 11:03:58 -0700

    finished project

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const imgs = document.querySelectorAll('#imgs img')
+const imgs = document.getElementById('imgs')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
+const img = document.querySelectorAll('#imgs img')
+
+let imageIndex = 0
+
+let interval = setInterval(run, 5000)
+
+function run() {
+  imageIndex++
+  changeImage()
+}
+
+function changeImage() {
+  if (imageIndex > img.length - 1) {
+    imageIndex = 0
+  } else if (imageIndex < 0) {
+    imageIndex = img.length - 1
+  }
+
+  imgs.style.transform = `translateX(${-imageIndex * 500}px)`
+}
+
+function resetInterval() {
+  clearInterval(interval)
+  interval = setInterval(run, 5000)
+}
 
 leftBtn.addEventListener('click', () => {
-    console.log(imgs)
-    imgs.forEach((img, idx) => {
-        console.log(img);
-    })
+  imageIndex--
+  changeImage()
+  resetInterval()
 })
 rightBtn.addEventListener('click', () => {
-
+  imageIndex++
+  changeImage()
+  resetInterval()
 })
